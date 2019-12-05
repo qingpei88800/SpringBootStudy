f657759d61fb3014a63879a1157846cb2f83dc11
liuqingpei <Liu74542392>
2019-12-05 15:54:44 +0800

    Initial commit

---
+++ src/test/java/com/lqp/study/spring/boot/SpringBootStudyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lqp.study.spring.boot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootStudyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
